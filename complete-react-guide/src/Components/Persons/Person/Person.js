9f73029673c3a004be0ed60b012e856d2db64a98
Yalagala <gurudev96@gmail.com>
2020-06-10 22:52:27 +0530

    Auxiliary

--- complete-react-guide/src/Components/Persons/Person/Person.js
+++ complete-react-guide/src/Components/Persons/Person/Person.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import './Person.css'
+import Aux from "../../../hoc/Auxiliary";
 
 class Person extends Component {
-    render() {
+  render() {
 
-        console.log('[Person.js] Rendering...')
-        return [
-            // < div className="Person">
-            <p key="i1" onClick={this.props.click}>
-                I'm {this.props.name} and I am {this.props.age} years old
-                </p>,
-
-            <p key="i2"> {this.props.children}</p>,
-
-            <input
-                key="i3"
-                type="text"
-                onChange={this.props.changed}
-                value={this.props.name}
-            />
-            // </div >
-        ];
-    }
+    console.log('[Person.js] Rendering...')
+    return (
+      <Aux>
+        < div className="Person">
+          <p onClick={this.props.click}>
+            I'm {this.props.name} and I am {this.props.age} years old
+          </p>
+          <p > {this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            value={this.props.name}
+          />
+        </div >
+      </Aux>
+    );
+  }
 };
 
 export default Person; 
