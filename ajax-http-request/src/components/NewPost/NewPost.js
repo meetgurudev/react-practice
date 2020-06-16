cd4804016696714dc0a3d1ed505f3a2f36c427cc
Yalagala <gurudev96@gmail.com>
2020-06-17 00:55:50 +0530

    Get and Post requests

--- ajax-http-request/src/components/NewPost/NewPost.js
+++ ajax-http-request/src/components/NewPost/NewPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import axios from 'axios';
 
 class NewPost extends Component {
     state = {
@@ -9,20 +10,36 @@ class NewPost extends Component {
         author: 'Max'
     }
 
-    render () {
+    postDataHandle = () => {
+        const data = {
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.cypress.io/posts/', data)
+            .then(response => {
+                this.props.addedPost(response.data.id)// = () => {
+
+                //     console.log(response.data.id);
+                // }
+            });
+    }
+
+    render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
+                    <option value="Guru">Guru</option>
+                    <option value="Dev">Dev</option>
+                    <option value="Gurudev">Gurudev</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandle}>Add Post</button>
             </div>
         );
     }
--- ajax-http-request/src/containers/Blog/Blog.js
+++ ajax-http-request/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,46 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.cypress.io/posts/')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Guru'
+                    }
+                })
+                this.setState({ posts: updatedPosts });
+            });
+    }
+
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={this.postSelectedHandler.bind(this, post.id)} />
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
