4183ba06e72a12ae9ec844905eb98d577be66311
Yalagala <gurudev96@gmail.com>
2020-06-19 23:59:39 +0530

    Added home Routes

--- ajax-http-request/src/containers/Blog/Blog.js
+++ ajax-http-request/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -16,7 +17,12 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* Path is predefined */}
+                {/* Exact is path is sensitive. */}
+                {/* Render what should be rendered when this path is clicked. */}
+
+                <Route path="/" exact render={() => <div>Home</div>} />
+                <Route path="/" render={() => <div>Home 2</div>} />
             </div>
         );
     }
