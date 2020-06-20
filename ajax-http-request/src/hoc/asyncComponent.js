7cf856828b1dec63d13405b1a75e3e1124ac36a3
Yalagala <gurudev96@gmail.com>
2020-06-21 00:23:45 +0530

    Code clean up

--- ajax-http-request/src/hoc/asyncComponent.js
+++ ajax-http-request/src/hoc/asyncComponent.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 
 const asyncComponent = (importComponent) => {
     return class extends Component {
+        // Set a new state
         state = {
+            // where the component to be imported will sit.
             component: null
         }
 
+        //On Mount...
         componentDidMount() {
+            // Exec the import callack...
             importComponent().then(cmp => {
                 console.log(cmp)
+                // then set the state with the response...
                 this.setState({ component: cmp.default });
             });
         }
 
         render() {
+            // Store the state component.
             const C = this.state.component;
 
+            // See if defined and then spread the props if any, else return null.
             return C ? <C {...this.props} /> : null;
         }
     }
