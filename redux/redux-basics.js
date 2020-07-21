1cb1d2c8ad7ab08e444ae6e7e35c6bfe60c60697
Yalagala <gurudev96@gmail.com>
2020-07-22 00:06:53 +0530

    React Start

---
+++ redux/redux-basics.js
@@ -0,0 +1,21 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+// Reducer
+// current state and action
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+
+
+// Subscription
