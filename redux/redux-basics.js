e158b6418de0488f586b12be5dd9adf074f45d5e
Yalagala <gurudev96@gmail.com>
2020-07-22 00:15:35 +0530

    Reducerm Store and Dispatching action

--- redux/redux-basics.js
+++ redux/redux-basics.js
@@ -8,6 +8,19 @@ const initialState = {
 // Reducer
 // current state and action
 const rootReducer = (state = initialState, action) => {
+    if (action.type === "INC_COUNTER") {
+        return {
+            ...state, // 
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === "ADD_COUNTER") {
+        return {
+            ...state, // 
+            counter: state.counter + action.value
+        };
+    }
     return state;
 }
 
@@ -16,6 +29,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
-
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
 
 // Subscription
