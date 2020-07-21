00eb6124aa0947769cb54d1ce9e9f86e4fe4985b
Yalagala <gurudev96@gmail.com>
2020-07-22 00:21:35 +0530

    Subscribe code

--- redux/redux-basics.js
+++ redux/redux-basics.js
@@ -28,9 +28,12 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
+
 // Dispatching Action
 store.dispatch({ type: 'INC_COUNTER' });
 store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
-
-// Subscription
